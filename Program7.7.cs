4830c94dd60be3e760a96f73db4a7fa9fb784a5f
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-10-24 12:43:42 +0400

    test8

---
+++ Program7.7.cs
@@ -0,0 +1,69 @@
+﻿// *,/,+,-,^, sqrt, sin, cos, tan, ctan
+double a, b, c, d;
+string str;
+Console.WriteLine("Enter operand: +,-,*,/,^,sqrt,sin,cos,tan,ctan ");
+str = Console.ReadLine();
+if(str == "sqrt"){
+Console.WriteLine("Enter c: ");
+c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(c));}
+
+else if(str == "sin"){
+    Console.WriteLine("Enter d: ");
+    d = Convert.ToDouble(Console.ReadLine());
+    double radian = d * Math.PI / 180;
+    Console.WriteLine(Math.Sin(radian));
+}
+else if(str == "cos"){
+    Console.WriteLine("Enter d: ");
+    d = Convert.ToDouble(Console.ReadLine());
+    double radian = d * Math.PI / 180;
+    Console.WriteLine(Math.Cos(radian));
+}
+else if(str == "tan"){
+    Console.WriteLine("Enter d: ");
+    d = Convert.ToDouble(Console.ReadLine());
+    double radian = d * Math.PI / 180;
+    Console.WriteLine(Math.Tan(radian));
+}
+else if(str == "ctan"){
+    Console.WriteLine("Enter d: ");
+    d = Convert.ToDouble(Console.ReadLine());
+    double radian = d * Math.PI / 180;
+    Console.WriteLine(1/Math.Tan(radian));
+}
+else if(str == "+"){
+    Console.WriteLine("Enter a: ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b: ");
+    b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(a + b);
+}
+else if(str == "-"){
+    Console.WriteLine("Enter a: ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b: ");
+    b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(a - b);
+}
+else if(str == "*"){
+    Console.WriteLine("Enter a: ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b: ");
+    b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(a * b);
+}
+else if(str == "/"){
+    Console.WriteLine("Enter a: ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b: ");
+    b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(a / b);
+}
+else if(str == "^"){
+    Console.WriteLine("Enter a: ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter b: ");
+    b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine(Math.Pow(a,b));
+}
